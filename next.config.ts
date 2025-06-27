d0fb6345c25f97016685df2017ee9ec28f83740e
Shane Friedman <smoores-gpg@friedmans.us>
2025-06-27 08:36:34 -0400

    Fix issue with cached tailwind utility classes

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,33 @@ const nextConfig: NextConfig = {
   output: "export",
   ...(process.env.NODE_ENV === "production" && { distDir: "build" }),
   trailingSlash: true,
+  webpack: (config, { dev }) => {
+    if (!dev) {
+      // Disable caching for postcss in production
+      // Without this, the TailwindCSS output gets
+      // cached and newly added utility classes
+      // don't show up in the production bundle
+      const rules = config.module.rules.find(
+        (rule: any) => typeof rule.oneOf === "object",
+      );
+      if (rules) {
+        rules.oneOf.forEach((rule: any) => {
+          if (rule.use && Array.isArray(rule.use)) {
+            rule.use.forEach((use: any) => {
+              if (use.loader && use.loader.includes("postcss-loader")) {
+                use.options = {
+                  ...use.options,
+                  sourceMap: false,
+                  cacheDirectory: false,
+                };
+              }
+            });
+          }
+        });
+      }
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
