5b573d48c8961f705bdc12bbaf8f89c5f02423aa
Shane Friedman <smoores-gpg@friedmans.us>
2025-06-27 23:30:08 -0400

    Add canonical URL to syndicated content

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,6 +1,7 @@
 import { Link } from "@/components/blog/Link";
 import { BlogSection } from "@/components/blog/Section";
 import { posts } from "@/posts/posts";
+import { Metadata } from "next";
 import { notFound } from "next/navigation";
 
 interface Props {
@@ -48,4 +49,16 @@ export async function generateStaticParams() {
   return posts.map((post) => ({
     slug: post.slug,
   }));
+}
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const { slug } = await params;
+  const post = posts.find((post) => post.slug === slug);
+  if (!post) notFound();
+
+  return {
+    title: post.title,
+    description: post.snippet,
+    ...(post.canonical && { alternates: { canonical: post.canonical } }),
+  };
 }
